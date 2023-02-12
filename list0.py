02246ae15e4751b77ade3d3c50b767f6470569c5
ㅇ ㅠ ㄴ ㅅ ㅐ ㅁ <48478079+PhoebeYoon@users.noreply.github.com>
2023-02-12 11:30:11 +0900

    Add files via upload

---
+++ list0.py
@@ -0,0 +1,21 @@
+# let's see the Python code for demonstrating the List Data Structure.   
+     
+# First, we will create the List  
+List = []  
+print("JavaTpoint data List: ")  
+print(List)  
+     
+# we are creating the List of numbers  
+List1 = [12, 24, 64, 18, 3, 201, 65, 35, 27, 29, 58, 42, 87, 30, 28, 79, 4, 90]  
+print("\n The JavaTpoint List of numbers: ")  
+print(List1)  
+     
+# Now, we will create the List of strings and will access it using index method.  
+  
+List2 = ["let's", "learn", "Python", "from", "JavaTpoint"]  
+print("\nList Items: ")  
+print(List2[0])   
+print(List2[2])  
+print(List2[1])  
+print(List2[4])  
+print(List2[3])  
