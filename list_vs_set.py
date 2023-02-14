f5191a180d5c1711880f941d25decaa7018ff948
ㅇ ㅠ ㄴ ㅅ ㅐ ㅁ <48478079+PhoebeYoon@users.noreply.github.com>
2023-02-14 14:22:44 +0900

    Update list_vs_set.py

--- list_vs_set.py
+++ list_vs_set.py
@@ -1,12 +1,12 @@
 # Now we will check is list are ordered  
   
-List1 = [9, 3, 6, 19, 67, "Hey", "JavaTpoint", 78, 2, 1]  
-List2 = [9, 3, 6, 19, 67, 78, "Hey", "JavaTpoint", 2, 1]  
+List1 = [9, 3, 6, 19, 67, "Hey", "Python", 78, 2, 1]  
+List2 = [9, 3, 6, 19, 67, 78, "Hey", "Python", 2, 1]  
 print("List1 = ", List1)  
 print("List2 = ", List2)  
 print('List1 == List2 ? ', List1 == List2)
 
 
-setB= set(["let's", "learn", "Python", "from", "JavaTpoint",10]) 
-setC= set(["learn","let's",  "Python", 10,"JavaTpoint", "from"]) 
+setB= set(["let's", "learn", "Python", "from", "Java",10]) 
+setC= set(["learn","let's",  "Python", 10, "Java", "from"]) 
 print('SetB == SetC ? ', setB == setC)
