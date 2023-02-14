7cbcc93f2a5a54c998f50cee394fcb5106c0cc38
ㅇ ㅠ ㄴ ㅅ ㅐ ㅁ <48478079+PhoebeYoon@users.noreply.github.com>
2023-02-14 14:19:50 +0900

    Update list0.py

--- list0.py
+++ list0.py
@@ -2,12 +2,12 @@
      
 # First, we will create the List  
 List = []  
-print("JavaTpoint data List: ")  
+print("The data List: ")  
 print(List)  
      
 # we are creating the List of numbers  
 List1 = [12, 24, 64, 18, 3, 201, 65, 35, 27, 29, 58, 42, 87, 30, 28, 79, 4, 90]  
-print("\n The JavaTpoint List of numbers: ")  
+print("\n The List of numbers: ")  
 print(List1)  
      
 # Now, we will create the List of strings and will access it using index method.  
