6b9e3198f597fa522cad6fe213f7dbf50fc266f0
ㅇ ㅠ ㄴ ㅅ ㅐ ㅁ <48478079+PhoebeYoon@users.noreply.github.com>
2023-02-21 22:13:45 +0900

    Update enumerate설명.py

--- enumerate설명.py
+++ enumerate설명.py
@@ -8,3 +8,10 @@ for letter  in enumerate(['a','b','c']):
 #인덱스 번호가 보인다. 이제 내용과 인덱스번호를 따로 가져오고 싶다면
 for  i, letter in enumerate(['a','b','c']):
     print('인덱스{}  내용 {}'.format(i , letter) )
+    
+for letter  in enumerate(['a','b','c'], start=1):
+    print(letter) 
+    
+for letter  in enumerate(['a','b','c'], start=11):
+    print(letter) 
+# start=숫자 를 이용해 시작번호를 지정할 수도 있습니다
