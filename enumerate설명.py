1e7b054a59a99685c0a869f4d5dcdbd043dde21c
ㅇ ㅠ ㄴ ㅅ ㅐ ㅁ <48478079+PhoebeYoon@users.noreply.github.com>
2023-02-21 22:17:01 +0900

    Update enumerate설명.py

--- enumerate설명.py
+++ enumerate설명.py
@@ -5,6 +5,8 @@ for letter  in ['a','b','c']:
 for letter  in enumerate(['a','b','c']):
     print(letter) 
     
+# 결과가 튜플 형태인 것을 확인하세요
+
 #인덱스 번호가 보인다. 이제 내용과 인덱스번호를 따로 가져오고 싶다면
 for  i, letter in enumerate(['a','b','c']):
     print('인덱스{}  내용 {}'.format(i , letter) )
@@ -12,6 +14,8 @@ for  i, letter in enumerate(['a','b','c']):
 for letter  in enumerate(['a','b','c'], start=1):
     print(letter) 
     
+#
 for letter  in enumerate(['a','b','c'], start=11):
     print(letter) 
 # start=숫자 를 이용해 시작번호를 지정할 수도 있습니다
+
